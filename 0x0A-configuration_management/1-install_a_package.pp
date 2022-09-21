339f0283d7beda080ace08650dadf828e2e2990e
Chioma2000 <jozinnachioma@gmail.com>
2022-09-20 22:23:25 -0700

    updating flask code

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# Install package
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+# Installing a package from pip3 using puppet script
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
